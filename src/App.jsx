555d3975ec3928b57626812ad697a8c2ba6aa125
shahola <snisha.athena@gmail.com>
2026-08-13 12:32:36 +0600

    Add Basic Bank Magpie route

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,21 @@ import { AnimatePresence, motion } from 'framer-motion'
 import Home from './pages/Home'
 import CaseStudyPage from './pages/CaseStudyPage'
 import MerchantOnboardingCaseStudyPage from './pages/MerchantOnboardingCaseStudyPage'
+import BasicBankMagpieCaseStudyPage from './pages/BasicBankMagpieCaseStudyPage'
 
 const pageVariants = {
   initial: { opacity: 0, filter: 'blur(6px)' },
   animate: { opacity: 1, filter: 'blur(0px)', transition: { duration: 0.45, ease: [0.22, 1, 0.36, 1] } },
-  exit:    { opacity: 0, filter: 'blur(6px)', transition: { duration: 0.3,  ease: [0.22, 1, 0.36, 1] } },
+  exit:    { opacity: 0, filter: 'blur(6px)', transition: { duration: 0.3, ease: [0.22, 1, 0.36, 1] } },
 }
-
-function PageWrapper({ children }) {
-  return (
-    <motion.div variants={pageVariants} initial="initial" animate="animate" exit="exit">
-      {children}
-    </motion.div>
-  )
-}
-
+function PageWrapper({ children }) { return <motion.div variants={pageVariants} initial="initial" animate="animate" exit="exit">{children}</motion.div> }
 function AnimatedRoutes() {
   const location = useLocation()
-  return (
-    <AnimatePresence mode="wait" initial={false}>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-        <Route path="/case-study/bkb-mobile" element={<PageWrapper><CaseStudyPage /></PageWrapper>} />
-        <Route path="/case-study/merchant-onboarding" element={<PageWrapper><MerchantOnboardingCaseStudyPage /></PageWrapper>} />
-      </Routes>
-    </AnimatePresence>
-  )
+  return <AnimatePresence mode="wait" initial={false}><Routes location={location} key={location.pathname}>
+    <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+    <Route path="/case-study/bkb-mobile" element={<PageWrapper><CaseStudyPage /></PageWrapper>} />
+    <Route path="/case-study/merchant-onboarding" element={<PageWrapper><MerchantOnboardingCaseStudyPage /></PageWrapper>} />
+    <Route path="/case-study/basic-bank-magpie" element={<PageWrapper><BasicBankMagpieCaseStudyPage /></PageWrapper>} />
+  </Routes></AnimatePresence>
 }
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <AnimatedRoutes />
-    </BrowserRouter>
-  )
-}
+export default function App() { return <BrowserRouter><AnimatedRoutes /></BrowserRouter> }
